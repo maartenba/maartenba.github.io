7c1235fc23fc617ee97b68188188e0f97e44cf4f
Maarten Balliauw <maarten@maartenballiauw.be>
2026-06-23 06:56:57 +0100

    Upgrade to Astro v7, enable `compressHTML`, and update dependencies.

--- astro.config.ts
+++ astro.config.ts
@@ -20,6 +20,7 @@ import path from "node:path";
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
+  compressHTML: true, // Keep v6 whitespace behavior (v7 defaults to 'jsx' which strips spaces between inline elements)
   integrations: [
     redirectFrom({
       contentDir: "src/data/blog",
@@ -64,10 +65,6 @@ export default defineConfig({
     }),
   },
   vite: {
-    // eslint-disable-next-line
-    // @ts-ignore
-    // This will be fixed in Astro 6 with Vite 7 support
-    // See: https://github.com/withastro/astro/issues/14030
     plugins: [tailwindcss()],
     optimizeDeps: {
       exclude: ["@resvg/resvg-js"],
